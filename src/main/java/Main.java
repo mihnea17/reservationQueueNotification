d0b2218ae5db25e48efd8ce40c3c18996ec092d7
Mihnea Patentasu <mihnea_pat@yahoo.com>
2018-10-14 11:31:13 +0300

    "First sketch of the offline business logic"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,8 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args){
         Restaurant cantuccio = new Restaurant("Cantuccio", 1);
-        Client firstClientInQueue = new Client(1, "Mihnea Patentasu", "0784291808", LocalDate.now());
-        Client secondClientInQueue = new Client(2, "Victor Patentasu", "0784291809", LocalDate.now());
+        Client firstClientInQueue = new Client(1, "Mihnea Patentasu", "0123", LocalDate.now());
+        Client secondClientInQueue = new Client(2, "Victor Patentasu", "0124", LocalDate.now());
         cantuccio.addClient(firstClientInQueue);
         cantuccio.addClient(secondClientInQueue);
         cantuccio.notifyNewFirstAndSecondClients();
